b6c6990b567ab6e7f60c1913af3597be1215e323
Mahsa <mahsa.ghezel.1995@gmail.com>
2023-07-08 10:52:17 +0330

    Unit conversion

--- src/app.js
+++ src/app.js
@@ -24,8 +24,10 @@ function formattedDate(timestamp) {
 function displayTemperature(response) {
   console.log(response.data);
 
+  celsiusTemperature = response.data.temperature.current;
+
   let temperatureElement = document.querySelector('#temperature');
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 
   let city = document.querySelector('#city');
   city.innerHTML = response.data.city;
@@ -78,3 +80,29 @@ function setBackgroundImage() {
   }
 }
 window.addEventListener('load', setBackgroundImage);
+
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  celsiusLink.classList.remove('active');
+  fahrenheitLink.classList.add('active');
+  fahrenheitTemperature = Math.round(fahrenheitTemperature);
+  let temperature = document.querySelector('#temperature');
+  temperature.innerHTML = fahrenheitTemperature;
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperature = document.querySelector('#temperature');
+  fahrenheitLink.classList.remove('active');
+  celsiusLink.classList.add('active');
+  temperature.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector('#fahrenheit-link');
+fahrenheitLink.addEventListener('click', showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector('#celsius-link');
+celsiusLink.addEventListener('click', showCelsiusTemperature);
